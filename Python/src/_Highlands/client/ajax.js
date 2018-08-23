d43746f453de3b85f4a0ce8b63787b897e5dab0f
Chris Seddon <seddon-software@keme.co.uk>
2018-08-23 14:56:05 +0100

    experimenting with AJAX progress

--- Python/src/_Highlands/client/ajax.js
+++ Python/src/_Highlands/client/ajax.js
@@ -11,7 +11,15 @@ function getAjaxData(url, fn) {
         dataType:'json',
         success: function(data) {
         	fn(data);
-        }	
+        },	
+    	progress: function(evt) {
+        if (evt.lengthComputable) {
+            console.log("Loaded " + parseInt( (evt.loaded / evt.total * 100), 10) + "%");
+        }
+        else {
+            console.log("Length not computable.");
+        }
+    }
     });
 }
 
